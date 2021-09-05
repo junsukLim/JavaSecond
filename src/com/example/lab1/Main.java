030f8230d8f39b8d0be8ac6ad6c77871d2c615de
junsukLim <junsuk9874@naver.com>
2021-09-05 17:38:41 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.print("What is your name?");
+		System.out.print("What is your nickname?");
 		String name = s.next();
-		System.out.print("Your name is "+name);
+		System.out.print("Your nickname is "+name);
 	}
 }
